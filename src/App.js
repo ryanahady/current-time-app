08bf37e4a732449d2e9a8a373694acb42a36205a
Ryan A <ryan.zad@gmail.com>
2021-03-23 10:59:25 +1100

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  let altText;
+  let imgSrc; 
+  let greetings;
+
+  const now = new Date().toLocaleTimeString();
+  const dateNow = new Date().toLocaleDateString();
+  const currentHours = new Date().getHours(); 
+
+  const [time, setTime] = useState(now);
+  const [date, setDate] = useState(dateNow);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  //upadate time each second 
+  setInterval(updateTime, 1000);
+  setInterval(updateDate, 1000);
+
+  function updateDate() {
+    const newDate = new Date().toLocaleDateString();
+    setDate(newDate);
+  }
+
+  function updateIconHours(){
+    setInterval(currentHours, 1000);
+  }
+  
+  if(currentHours < 12){
+    greetings = 'Good Morning!';
+    imgSrc = require('./morning.png').default;
+    altText = "morning-image";
+  }else if(currentHours < 18){
+    greetings = 'Good Afternoon!';
+    imgSrc = require('./afternoon.png').default;
+    altText = "afternoon-image";
+  }else{
+    greetings = 'Good Night!';
+    imgSrc = require('./night.png').default;
+    altText = "night-image";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="rowTop"><img src={imgSrc} alt={altText}/></div>
+      <div className="row"><h1 style={{ paddingTop: "5%" }}>{greetings}</h1></div>
+      <div className="row"><h1 style={{ paddingTop: "10%" }}>{date}</h1></div>
+      <div className="row"><h1 style={{ paddingTop: "10%" }}>{time}</h1></div>
     </div>
   );
 }
